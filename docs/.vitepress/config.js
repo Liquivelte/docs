72fa95a5fd8337c797d8de3bdcd02020c755e84c
m.ali petek <malipetek@gmail.com>
2023-03-07 13:33:12 +0300

    build commit

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,55 +1,11 @@
-const shiki = require('shiki');
 const path = require('path');
-
-// Thanks https://rodneylab.com/sveltekit-shiki-syntax-highlighting/
-function escapeHtml(code) {
-  return code.replace(
-    /[{}`]/g,
-    (character) => ({ '{': '&lbrace;', '}': '&rbrace;', '`': '&grave;' }[character]),
-  );
-}
-let liquivelteHighlihter, svelteHighlihter;
-(async () => {
-  liquivelteHighlihter = await shiki.getHighlighter({
-    theme: 'github-dark',
-    langs: [
-      {
-        id: 'liquivelte',
-        scopeName: 'source.liquivelte',
-        path: path.resolve(__dirname, './liquivelte.tmLanguage.json')
-      },
-      'javascript',
-      'css',
-      'liquid',
-      'scss',
-      'sass',
-      'less'
-    ]
-  });
-  svelteHighlihter = await shiki.getHighlighter({
-    theme: 'github-dark',
-    langs: [
-      {
-        id: 'svelte',
-        scopeName: 'source.svelte',
-        path: path.resolve(__dirname, './svelte.tmLanguage.json')
-      },
-      'javascript',
-      'css',
-      'liquid',
-      'scss',
-      'sass',
-      'less'
-    ]
-  });
-})();
 
 export default {
   title: 'Liquivelte | Docs',
   description: 'Liquivelte Documentation & Examples',
   lang: 'en-US',
   themeConfig: {
-      logo: '/public/liquivelte.png',
+      logo: '/liquivelte.png',
       sidebar: [
         {
           text: 'Setup',
@@ -70,13 +26,22 @@ export default {
   },
   markdown: {
     theme: 'github-dark',
-    highlight: function (str, lang) {
-      if (lang === 'liquivelte') {
-        return escapeHtml(liquivelteHighlihter.codeToHtml(str, { lang: 'liquivelte' }));
-      }
-      if (lang === 'svelte') {
-        return escapeHtml(svelteHighlihter.codeToHtml(str, { lang: 'svelte' }));
-      }
-    }
+    languages: [{
+      id: 'liquivelte',
+      scopeName: 'source.liquivelte',
+      path: path.resolve(__dirname, './liquivelte.tmLanguage.json')
+    },
+    {
+      id: 'svelte',
+      scopeName: 'source.svelte',
+      path: path.resolve(__dirname, './svelte.tmLanguage.json')
+    },
+      'javascript',
+      'css',
+      'liquid',
+      'scss',
+      'sass',
+      'less'
+    ]
   }
 };
